e8dc552e137bf2324102f71dfe7cc671fe965454
Matthew <matthew.97.clark@gmail.com>
2019-10-25 17:33:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,16 @@ end
 def pop_with_args(array)
   return array.pop(2);
 end
+
+def using_shift(array)
+  return array.shift  
+end
+
+def shift_with_args(array)
+  return array.shift(2)  
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
